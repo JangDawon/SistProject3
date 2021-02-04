ec0162861758043d4d338ecd2971194615c0d984
junseong <junseong@pc>
2021-02-04 13:47:01 +0900

    3

--- src/main/java/com/project3/vo/IdusSessionVO.java
+++ src/main/java/com/project3/vo/IdusSessionVO.java
@@ -2,8 +2,20 @@ package com.project3.vo;
 
 public class IdusSessionVO {
 	int result;
-	String uname;
+	String uemail, upass, uname, cp, addr1, addr2, addr3, aggrsms, aggremail, udate, pfile, psfile;
 	
+	public String getPfile() {
+		return pfile;
+	}
+	public void setPfile(String pfile) {
+		this.pfile = pfile;
+	}
+	public String getPsfile() {
+		return psfile;
+	}
+	public void setPsfile(String psfile) {
+		this.psfile = psfile;
+	}
 	public int getResult() {
 		return result;
 	}
@@ -12,6 +24,60 @@ public class IdusSessionVO {
 	}
 	public String getUname() {
 		return uname;
+	}
+	public String getUemail() {
+		return uemail;
+	}
+	public void setUemail(String uemail) {
+		this.uemail = uemail;
+	}
+	public String getUpass() {
+		return upass;
+	}
+	public void setUpass(String upass) {
+		this.upass = upass;
+	}
+	public String getCp() {
+		return cp;
+	}
+	public void setCp(String cp) {
+		this.cp = cp;
+	}
+	public String getAddr1() {
+		return addr1;
+	}
+	public void setAddr1(String addr1) {
+		this.addr1 = addr1;
+	}
+	public String getAddr2() {
+		return addr2;
+	}
+	public void setAddr2(String addr2) {
+		this.addr2 = addr2;
+	}
+	public String getAddr3() {
+		return addr3;
+	}
+	public void setAddr3(String addr3) {
+		this.addr3 = addr3;
+	}
+	public String getAggrsms() {
+		return aggrsms;
+	}
+	public void setAggrsms(String aggrsms) {
+		this.aggrsms = aggrsms;
+	}
+	public String getAggremail() {
+		return aggremail;
+	}
+	public void setAggremail(String aggremail) {
+		this.aggremail = aggremail;
+	}
+	public String getUdate() {
+		return udate;
+	}
+	public void setUdate(String udate) {
+		this.udate = udate;
 	}
 	public void setUname(String uname) {
 		this.uname = uname;
