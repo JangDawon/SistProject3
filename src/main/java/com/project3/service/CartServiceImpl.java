38b7152ca40b50417c0cd231f08561fa7707c851
JangDawon <wkdekdnjs25@naver.com>
2021-02-08 13:00:30 +0900

    수정

--- src/main/java/com/project3/service/CartServiceImpl.java
+++ src/main/java/com/project3/service/CartServiceImpl.java
@@ -22,32 +22,7 @@ public class CartServiceImpl implements CartService {
 	}
 	
 	
-	@Override
-	public String getCartList(String uemail) {
-		ArrayList<IdusCartVO> list = cartDAO.getCartList(uemail);
-		
-		JsonArray jarray = new JsonArray();
-		JsonObject jdata = new JsonObject();
-		Gson gson = new Gson();
-		
-		for(IdusCartVO vo : list) {
-			JsonObject jobj = new JsonObject();
-			
-			jobj.addProperty("cid", vo.getCid());
-			jobj.addProperty("uemail", vo.getUemail());
-			jobj.addProperty("pid", vo.getPid());
-			jobj.addProperty("p_name", vo.getP_name());
-			jobj.addProperty("p_qty", vo.getP_qty());
-			jobj.addProperty("p_opt", vo.getP_opt());
-			jobj.addProperty("p_price", vo.getP_price());
-			
-			jarray.add(jobj);
-		}
-
-		jdata.add("jlist", jarray);
-		return gson.toJson(jdata);
-		
-	}
+	
 	
 	
 }
